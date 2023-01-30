97a922340bc8bfcbd9630abc41239a3dec4cc15f
Rohit Rai <rohitrai@ibm.com>
2023-01-31 00:40:57 +0530

    loop.go

---
+++ loop.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// init ; condition ; post {
+
+	for i := 0; i <= 5; i++ {
+		fmt.Println("this is the value of i", i)
+	}
+}
+
+//this is the value of i 0
+//this is the value of i 1
+//this is the value of i 2
+//this is the value of i 3
+//this is the value of i 4
+//this is the value of i 5

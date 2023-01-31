d6709b7c4bde370d4e5ebe5df109ca439f07dc13
Rohit Rai <rohitrai@ibm.com>
2023-01-31 17:28:14 +0530

    nested_loops.go

---
+++ nested_loops.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 4; i++ {
+		for j := 1; j <= 2; j++ {
+
+			fmt.Println(i, j)
+		}
+
+	}
+}
+
+//0 1
+//0 2
+//1 1
+//1 2
+//2 1
+//2 2
+//3 1
+//3 2
+//4 1
+//4 2
